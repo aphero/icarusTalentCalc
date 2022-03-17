db67738d6dffda1e56675d04afb1010cc204636c
McJosh <jmckelvey0@gmail.com>
2022-03-17 12:53:06 -0400

    Use liquidjs instead of lit-html

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,17 @@ import * as path from 'path'
 const app = express()
 const port = 3000
 
+const a = html`<div>Hello! Greeting!</div>`
+
 app.use(express.static('img')) // for images
 app.use('/img', express.static('img'))
-app.use(express.static('templates')) // for CSS 
+app.use(express.static('templates')) // for CSS  
 
 app.get('/', (req, res) => {
   console.log(`Request received, headers: ${JSON.stringify(req.headers)}`)
   console.log(`Rendering: ${path.resolve("templates/index.htm")}`)
-  res.sendFile(path.resolve("templates/index.htm"))
+  // res.sendFile(path.resolve("templates/index.htm"))
+  res.send(a)
 })
 
 app.listen(port, () => {
